6c18e733efc5f2b1eab92636d71b56f6c9e34a11
unknown <libojian1234@126.com>
2020-05-22 23:17:43 +0800

    part1笔记和作业

---
+++ nodes/03-03-ts/dist/07-enym-types.js
@@ -0,0 +1,19 @@
+"use strict";
+//枚举（Enum）
+Object.defineProperty(exports, "__esModule", { value: true });
+// enum PostStatus {  //如果后面不赋值，就是根据第一个值依次加1
+//     Draft = 3,      //3,4,5
+//     Unpubliced,
+//     Public
+// }
+// enum PostStatus {  //如果是字符串，那每个都要赋值
+//     Draft = 'add',      //3,4,5
+//     Unpubliced = 'bar',
+//     Public = 'asd'
+// }
+var post = {
+    title: 'Hello TypeScript',
+    content: 'TypeScript is a typed superset of JavaScript',
+    status: 0 /* Draft */
+};
+//# sourceMappingURL=07-enym-types.js.map
